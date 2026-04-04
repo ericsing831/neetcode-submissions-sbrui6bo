05c4da1d225f07606d8e229cafc83a7051d775f1
ericsing831 <70386374+ericsing831@users.noreply.github.com>
2026-04-04 04:17:14 -0400

    Add: find-target-in-rotated-sorted-array - submission-4

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-4.cs
@@ -0,0 +1,39 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+        int l = 0, r = nums.Length - 1;
+        while (l <= r) {
+            int m = (l + r) / 2;
+            if (nums[m] == target) return m;
+            if (nums[l] <= nums[m]) { // a sorted segment
+                if (nums[l] <= target && target < nums[m]) { //target in sorted segment
+                    r = m - 1;
+                    goto SecondSeach;
+                }
+                else 
+                    l = m + 1;
+            }
+            else { // a ratated segment 
+                if (nums[m] < target && target <= nums[r]) 
+                    l = m + 1;
+                else 
+                    r = m - 1;
+            }
+        }
+        return -1;
+
+        SecondSeach:
+            while (l <= r) {
+                int m = (l + r) / 2;
+                if (nums[m] > target) {
+                    r = m - 1;
+                }
+                else if (nums[m] < target) {
+                    l = m + 1;
+                }
+                else {
+                    return m;
+                }
+            }
+        return -1;
+    } 
+}

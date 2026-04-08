e49477d4e970ff2dba5a0015e5db6066b983f8bb
ericsing831 <70386374+ericsing831@users.noreply.github.com>
2026-04-07 22:52:55 -0400

    Add: capacity-to-ship-packages-within-d-days - submission-0

---
+++ Data Structures & Algorithms/capacity-to-ship-packages-within-d-days/submission-0.cs
@@ -0,0 +1,34 @@
+public class Solution {
+    public int ShipWithinDays(int[] weights, int days) {
+        int l = weights.Max(), r = weights.Sum();
+        int res = 0;
+        int i = 0;
+        while (l <= r) {
+            int m = (l + r) / 2;
+
+            int cap = m;
+            int d = 1;
+            bool canShip;
+            foreach (int weight in weights) {
+                //reset
+                if (cap < weight) {
+                    d++;
+                    cap = m;
+                }
+                cap -= weight;
+            }
+            canShip = (d <= days);
+
+            if (canShip) {
+                // update res
+                res = m;
+                // shift left to find limit
+                r = m - 1;
+            }
+            else {
+                l = m + 1;
+            }
+        }
+        return res;
+    }
+}

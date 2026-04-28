26fc1571dff9ee585c264aab091c0d2ccdcac5d9
ericsing831 <70386374+ericsing831@users.noreply.github.com>
2026-04-28 00:19:12 -0400

    Add: reverse-linked-list-ii - submission-2

---
+++ Data Structures & Algorithms/reverse-linked-list-ii/submission-2.cs
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode ReverseBetween(ListNode head, int left, int right) {
+        if (head.next == null) return head;
+        ListNode lConn = null;
+        ListNode prev = null;
+        ListNode curr = head;   
+        int index = 1;
+        // read left position
+        while (left > index) {
+            lConn = curr;
+            curr = curr.next;
+            index++;
+        }
+
+        ListNode rConn = null;
+        rConn = curr;
+        while (index <= right) {
+            var temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+            index++;
+        }
+        rConn.next = curr;
+        if (lConn == null) {
+            return prev;
+        }
+        else {
+            lConn.next = prev;
+            return head;
+        }
+    }
+}

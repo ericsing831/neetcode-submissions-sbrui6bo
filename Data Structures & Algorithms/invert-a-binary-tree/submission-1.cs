ff110beefe0c2aab7b2f554c61423008d1a586ee
ericsing831 <70386374+ericsing831@users.noreply.github.com>
2026-05-02 00:21:34 -0400

    Add: invert-a-binary-tree - submission-1

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-1.cs
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public TreeNode InvertTree(TreeNode root) {
+        var queue = new Queue<TreeNode>();
+
+        if (root != null) {
+            queue.Enqueue(root);
+        }
+
+        while (queue.Count > 0) {
+            var curr = queue.Dequeue();
+
+            // 🔥 THIS is the key missing logic
+            var temp = curr.left;
+            curr.left = curr.right;
+            curr.right = temp;
+
+            if (curr.left != null) {
+                queue.Enqueue(curr.left);
+            }
+            if (curr.right != null) {
+                queue.Enqueue(curr.right);
+            }
+        }
+
+        return root;
+    }
+}

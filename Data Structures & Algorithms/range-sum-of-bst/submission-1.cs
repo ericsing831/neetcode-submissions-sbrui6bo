2541a697f5e4c008f05b4c1137e9570621f02723
ericsing831 <70386374+ericsing831@users.noreply.github.com>
2026-06-13 17:40:58 -0400

    Add: range-sum-of-bst - submission-1

---
+++ Data Structures & Algorithms/range-sum-of-bst/submission-1.cs
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public int RangeSumBST(TreeNode root, int low, int high) {
+        int sum = 0;
+        void InOrder(TreeNode root) {
+            if (root == null) return;
+            if (root.val < low) {
+                InOrder(root.right);
+            }
+            else if (root.val > high) {
+                InOrder(root.left);
+            }
+            else {
+                sum += root.val;
+                InOrder(root.left);
+                InOrder(root.right);
+            }
+        }
+        InOrder(root);
+        return sum;
+    }
+}

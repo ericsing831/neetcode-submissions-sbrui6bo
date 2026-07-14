6ea7f5537d6a700b8eefcd0e42d76d72b7b8f85d
ericsing831 <70386374+ericsing831@users.noreply.github.com>
2026-07-14 01:59:51 -0400

    Add: serialize-and-deserialize-binary-tree - submission-1

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-1.cs
@@ -0,0 +1,73 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public string Serialize(TreeNode root) {
+        var queue = new Queue<TreeNode>();
+        var strList = new List<string>();
+
+        queue.Enqueue(root);
+        while (queue.Count != 0) {
+            var curr = queue.Dequeue();
+
+            if (curr == null) {
+                strList.Add("N");
+                continue;
+            }
+
+            strList.Add(curr.val.ToString());
+            queue.Enqueue(curr.left);
+            queue.Enqueue(curr.right);
+        }
+
+        return string.Join("|", strList);
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode Deserialize(string data) {
+        string[] dataArr = data.Split("|");
+
+        int traversed = 0;
+        var queue = new Queue<TreeNode>();
+        if (dataArr[0] == "N")
+        {
+            return null;
+        }
+
+        TreeNode root = new TreeNode(int.Parse(dataArr[traversed]));
+        queue.Enqueue(root);
+        traversed++;
+
+        while (queue.Count != 0 && traversed < dataArr.Length) {
+            var curr = queue.Dequeue();
+
+            if (dataArr[traversed] != "N") {
+                curr.left = new TreeNode(int.Parse(dataArr[traversed]));
+                traversed++;
+                queue.Enqueue(curr.left);
+            }
+            else traversed++;
+
+            if (dataArr[traversed] != "N") {
+                curr.right = new TreeNode(int.Parse(dataArr[traversed]));
+                traversed++;
+                queue.Enqueue(curr.right);
+            }
+            else traversed++;
+        }
+        return root;
+    }
+}

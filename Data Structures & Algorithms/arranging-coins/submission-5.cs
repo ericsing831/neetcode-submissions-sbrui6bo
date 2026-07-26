61b564ab485410b8c205ff55023c0f9ff43f1315
ericsing831 <70386374+ericsing831@users.noreply.github.com>
2026-07-26 16:22:20 -0400

    Add: arranging-coins - submission-5

---
+++ Data Structures & Algorithms/arranging-coins/submission-5.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int ArrangeCoins(int n) {
+        long left = 0;
+        long right = n;
+        long altMid = -1;
+        while (left <= right) {
+            long mid = (left - right) / 2 + right;
+            long coins = mid * (mid + 1) / 2;
+
+            if (coins == n) {
+                return (int)mid;
+            }
+            else if (coins > n) {
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        return (int)left - 1;
+    }
+}

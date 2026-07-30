db9a6465fa0d2edd017aa1289d6e26b9b1337432
ericsing831 <70386374+ericsing831@users.noreply.github.com>
2026-07-30 00:31:31 -0400

    Add: find-first-and-last-position-of-element-in-sorted-array - submission-0

---
+++ Data Structures & Algorithms/find-first-and-last-position-of-element-in-sorted-array/submission-0.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    public int[] SearchRange(int[] nums, int target) {
+        int left = 0;
+        int right = nums.Length - 1;
+
+        if (nums.Length == 0) return new int[] {-1, -1};
+        
+        while (left < right) {
+            int mid = (left + right) / 2; 
+            if (nums[mid] >= target) 
+                right = mid;
+            else 
+                left = mid + 1;
+        }
+        int resL = left;
+        left = 0;
+        right = nums.Length - 1;
+        while (left < right) {
+            int mid = left + (right - left + 1) / 2;
+            if (nums[mid] > target)
+                right = mid - 1;
+            else 
+                left = mid;
+        }
+        if (nums[resL] == nums[right] && nums[resL] == target)
+            return new int[2] {resL, right};
+        else return new int[2] {-1, -1};
+    }
+}

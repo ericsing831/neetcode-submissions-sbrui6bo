25ad039307b0eed62e13e1130c710042f4391186
ericsing831 <70386374+ericsing831@users.noreply.github.com>
2026-08-11 13:36:16 -0400

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        int[] res = null;
+        var dict = new Dictionary<int, (int integer, int index)>();
+        for (int i = 0; i < nums.Length; i++) {
+            dict[(target - nums[i])] = (nums[i], i);
+        }
+        for (int i = 0; i < nums.Length; i++) {
+            if (dict.ContainsKey(nums[i])) {
+                if ((dict[nums[i]].integer + nums[i] == target) && (i != dict[nums[i]].index))
+                    res = new int[] {i, dict[nums[i]].index};
+            }
+        }
+        Array.Sort(res);
+        return res;
+    }
+}

caf1384128a91cbf29f48e38eaaa399aba49c5f4
ericsing831 <70386374+ericsing831@users.noreply.github.com>
2026-08-12 13:32:52 -0400

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cs
@@ -0,0 +1,38 @@
+public class Solution {
+    public List<List<string>> GroupAnagrams(string[] strs) {
+        var res = new List<List<string>>();
+        int i = 1;
+        res.Add(new List<string>(){strs[0]});
+
+        while (i < strs.Length) {
+            bool added = false;
+            foreach (List<string> subRes in res) {
+                if (isAnagrams(subRes[0], strs[i])) {
+                    subRes.Add(strs[i]);
+                    added = true;
+                    break;
+                }
+            }
+            if (!added)
+                res.Add(new List<string>(){strs[i]});
+            i++;
+        }
+
+        return res;
+
+        bool isAnagrams(string a, string b) {
+            if (a.Length != b.Length) return false;
+            int offset = (int)'a';
+            int[] alphabets = new int[26];
+            for (int i = 0; i < a.Length; i++) {
+                alphabets[(int)a[i] - offset]++;
+                alphabets[(int)b[i] - offset]--;
+            }
+            foreach (int i in alphabets) {
+                if (i != 0) 
+                    return false;
+            }
+            return true;
+        }
+    }
+}

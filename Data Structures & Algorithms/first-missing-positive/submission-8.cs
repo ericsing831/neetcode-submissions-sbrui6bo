f4f0e0eba5ef9270e56d13bb1dfbab163928f3fc
ericsing831 <70386374+ericsing831@users.noreply.github.com>
2026-08-27 10:56:35 -0400

    Add: first-missing-positive - submission-8

---
+++ Data Structures & Algorithms/first-missing-positive/submission-8.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int FirstMissingPositive(int[] nums) {
+        for (int i = 0; i < nums.Length; i++) {
+            if (nums[i] < 0) 
+                nums[i] = 0;
+        }
+
+        for (int i = 0; i < nums.Length; i++) {
+            int idx = Math.Abs(nums[i]);
+            if (1 <= idx && idx < nums.Length + 1) {
+                if (nums[idx - 1] > 0) 
+                    nums[idx - 1] *= -1;
+                else if (nums[idx - 1] == 0)
+                    nums[idx - 1] =  -(nums.Length + 1);  
+            }
+        }
+
+        for (int i = 1; i < nums.Length + 1; i++) {
+            if (nums[i - 1] >= 0) 
+                return i;
+        }        
+        return nums.Length + 1;
+    }
+}
